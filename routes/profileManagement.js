41abfa550528f40399bc0da86a4ce97531f38dd9
maximusfurtado <158118471+maximusfurtado@users.noreply.github.com>
2024-03-29 18:49:04 -0700

    Add files via upload

--- routes/profileManagement.js
+++ routes/profileManagement.js
@@ -2,17 +2,17 @@ const express = require('express');
 const router = express.Router(); 
 const pool = require('./creds'); 
 
-async function updateUser(userID, fullname, add1, add2, city, state, zip, info_complete) {
+async function updateUser(user, fullname, add1, add2, city, state, zip, info_complete) {
     const result = await pool.query('UPDATE users SET fullName= ?, address1 = ? , address2 = ?, city = ? , state = ?, zip = ?, info_complete = ? WHERE username = ? ', [fullName, add1, add2, city, state, zip, info_complete, username])
     return result;
   }
-router.get('/profileManagement/:userID', async(req, res) =>{
-    const username = req.params.userID
+router.get('/profileManagement/:user', async(req, res) =>{
+    const username = req.params.user
     console.log(username + " accessed profile management"); 
 
 });  
 
-router.post('/profileManagement/:userID', async(req, res) => {
+router.post('/profileManagement/:user', async(req, res) => {
     const fname = req.body.fullname;
     const taddress1 = req.body.address1;
     const taddress2 = req.body.address2;
@@ -34,7 +34,7 @@ router.post('/profileManagement/:userID', async(req, res) => {
         curr_user.info_completed = true;
     
         //console.log(curr_user);
-        await updateUser(curr_user.userID, curr_user.fname, curr_user.add, curr_user.add2, curr_user.city, curr_user.state, curr_user.zip, curr_user.info_completed, curr_user.username);
+        await updateUser(curr_user.user, curr_user.fname, curr_user.add, curr_user.add2, curr_user.city, curr_user.state, curr_user.zip, curr_user.info_completed, curr_user.username);
         
         logged_in = true;
         res.redirect('/index');
@@ -43,5 +43,7 @@ router.post('/profileManagement/:userID', async(req, res) => {
         res.send('Please fill in all required fields');
       }
 
-});
+}); 
+
+module.exports = router; 
   
