5f87d276efca2ceccbed1a273ee99c3e4773259e
codewithdinh <thiendinhbui208@gmail.com>
2024-04-07 15:26:33 -0500

    suggestion on fuelForm

--- public/fuelForm.js
+++ public/fuelForm.js
@@ -1,11 +1,26 @@
-async function submitForm(event) {
-    event.preventDefault(); // Prevent default form submission
 
+addEventListener("DOMContentLoaded", function() {
+    document.querySelector("form").addEventListener("submit", function(event){
+        event.preventDefault(); // Prevent default form submission behavior
+        if (event.submitter.id === 'cal') {
+            // Calculate total logic
+            calPricePerGallon();
+        } else if (event.submitter.id === 'save') {
+            // Save quote logic
+            submitForm();
+        }
+    });
+ });
+
+//function to submit Form
+async function submitForm() {
+    // event.preventDefault(); // Prevent default form submission
+    console.log("Submitting the form...");
     // Get form data
     const gallons = document.getElementById('gallons').value;
     const deliveryAddress = document.getElementById('delivery-address').value;
     const deliveryDate = document.getElementById('delivery-date').value;
-    const pricePerGallon = document.getElementById('price-per-gallon').value;
+    // const pricePerGallon = document.getElementById('price-per-gallon').value; //this is filled by backend, not from user
 
     // Send form data to server
     const response = await fetch('http://localhost:3000/submitQuote', {
@@ -13,7 +28,8 @@ async function submitForm(event) {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ gallons, deliveryAddress, deliveryDate, pricePerGallon})
+        // body: JSON.stringify({ gallons, deliveryAddress, deliveryDate, pricePerGallon})
+        body: JSON.stringify({ gallons, deliveryAddress, deliveryDate})
     });
 
     if (response.ok) {
@@ -25,4 +41,27 @@ async function submitForm(event) {
     }
 }
 
-document.querySelector('form').addEventListener('submit', submitForm);
+// document.querySelector('form').addEventListener('submit', submitForm);
+
+//function to cal price per gallon
+async function calPricePerGallon()  {
+    
+    console.log("Calculating suggested price per gallon and total amount...");
+    try {
+        const response = await fetch(`http://localhost:3000/submitQuote`, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+    const price = await response.json();
+    const pricePerGallon = price.price;
+    const galreq = document.querySelector("#gallons").value;
+    total = price.price * galreq;
+    document.getElementById('price-per-gallon').value= `$${pricePerGallon.toFixed(2)}`;
+    document.getElementById('total-amount-due').value= `$${total.toFixed(2)}`;
+    } catch(error){
+        console.log(error);
+    };
+}
+
