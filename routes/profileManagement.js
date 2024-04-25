cd0aba01e183b1f30182ccfca4b73b9db8bd13d2
maximusfurtado <158118471+maximusfurtado@users.noreply.github.com>
2024-04-25 08:43:22 -0700

    Add files via upload

--- routes/profileManagement.js
+++ routes/profileManagement.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const pool = require('./creds');
+const pool = require('./creds'); 
+
+/*
+async function updateUser(username, fullname, add1, city, state, zip,) {
+    const result = await pool.query('UPDATE clientInfo SET fullname= ?, address1 = ? , city = ? , state = ?, zip = ?, WHERE username = ? ', [fullname, add1, city, state, zip, username])
+    return result;
+  }
+*/
 
 
 router.post('/:firstname/:lastname/:street/:city/:state/:zipcode', async (req, res) => {
@@ -34,4 +41,36 @@ router.post('/:firstname/:lastname/:street/:city/:state/:zipcode', async (req, r
     res.json();
 })
 
-module.exports = router
+module.exports = router 
+
+/* 
+router.post('/profile', async(req, res) => {
+    const fname = req.body.fullname;
+    const taddress1 = req.body.address1;
+    const zip = req.body.zipcode;
+    const city = req.body.city;
+    const state = req.body.state;
+    
+  
+    curr_user.fname = fname;
+    curr_user.add = taddress1;
+    curr_user.city = city;
+    curr_user.state = state;
+    curr_user.zip = zip;
+    curr_user.user_history = [];  
+
+    if (fname && taddress1 && zip && city && state) { // Check if all required fields are present
+        //console.log(curr_user);
+        await updateUser(curr_user.username, curr_user.fname, curr_user.add, curr_user.city, curr_user.state, curr_user.zip);
+        
+        logged_in = true;
+        res.redirect('/index');
+      } else {
+        // Inform user that all required fields are not present
+        res.send('Please fill in all required fields');
+      }
+
+}); 
+
+module.exports = router; 
+*/
